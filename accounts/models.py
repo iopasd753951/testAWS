a3bfd25d2307ff0f6b933c69f47baa7278f57c75
iopasd753951 <iopasd753951@gmail.com>
2020-05-19 04:38:10 +0900

    [수정] account --> accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Account(models.Model):
+class Accounts(models.Model):
     pk_id = models.AutoField(primary_key=True)
     email = models.EmailField(max_length=254, unique=True)
     nick_name = models.CharField(max_length=16, unique=True)
